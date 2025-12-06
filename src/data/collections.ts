af9b02cf2a5613aeca6f89378359388a832633da
Joyce Lin <ohanalin@gmail.com>
2025-12-06 11:37:09 -0500

    change ingredients

--- src/data/collections.ts
+++ src/data/collections.ts
@@ -4,10 +4,10 @@ export const products = [
     name: "Gentle Pup Soap",
     price: 11.99,
     category: "Natural",
-    scent:"TeaTree",
+    scent:"Eucalyptus, Pepperminy, Lavender",
     image: "images/products/dog.jpg",
     description: "A mild, pet-friendly soap bar made for dogs with sensitive skin, using gentle oils and natural ingredients.",
-    ingredients: "Olive Oil, Coconut Oil, Essential Oil, Sodium Hydroxide"
+    ingredients: "Olive Oil, Coconut Oil, Apple Cider"
   },
   {
     id: 2,
@@ -17,7 +17,7 @@ export const products = [
     scent:"Mint, Lemon",
     image: "images/products/mintsalt.jpg",
     description: "A revitalizing mint-infused bar that exfoliates gently with natural salt while delivering a crisp, cooling sensation.",
-    ingredients: "Olive Oil, Coconut Oil, Salt, French Clay, Essential Oil, Sodium Hydroxide"
+    ingredients: "Olive Oil, Coconut Oil, Salt, French Clay"
   },
   {
     id: 3,
@@ -26,7 +26,7 @@ export const products = [
     category: "Floral",
     image: "images/products/lavender.jpg",
     description: "A soothing lavender-infused bar designed to relax the mind and soften the skin with every bath.",
-    ingredients: "Olive Oil, Coconut Oil, Corn Flower, Purple Mica Powder, Milk, Essential Oil, Sodium Hydroxide"
+    ingredients: "Olive Oil, Coconut Oil, Corn Flower, Purple Mica Powder, Milk, Essential Oil"
   },
   {
     id: 4,
@@ -35,69 +35,76 @@ export const products = [
     category: "Herbal",
     image: "images/products/tea.jpg",
     description: "A calming green tea bar packed with antioxidants to soothe, nourish, and promote radiant skin.",
-    ingredients: "Olive Oil, Coconut Oil, Matcha Powder, Gold Mica, Essential Oil, Sodium Hydroxide"
+    ingredients: "Olive Oil, Coconut Oil, Matcha Powder, Gold Mica"
   },
   {
     id: 5,
     name: "Cherry Blossom Breeze",
     price: 12.99,
     category: "Floral",
+    scent: "Cherry Blossome",
     image: "images/products/sakura.jpg",
     description: "A light, airy cherry blossom scent that softens the skin and calms the senses with every wash.",
-    ingredients: "Olive Oil, Coconut Oil, Shea Butter, Sunflower Seed Oil, Essential Oil, French Pink Clay, Sodium Hydroxide"
+    ingredients: "Olive Oil, Coconut Oil, Shea Butter, Sunflower Seed Oil, French Pink Clay"
   },
   {
     id: 6,
     name: "Coffee Morning Scrub",
     price: 11.99,
     category: "Specialty",
+    scent: "Coffee",
     image: "images/products/coffee.jpg",
     description: "Awaken your senses with a rich, coffee-infused exfoliating bar that smooths and revitalizes the skin.",
-    ingredients: "Olive Oil, Beeswax, Coconut Oil, Sunflower Seed Oil, Shea Fruit Butter, Essential Oil, Cocoa Powder, Coffee Grounds, Sodium Hydroxide"
+    ingredients: "Olive Oil, Beeswax, Coconut Oil, Sunflower Seed Oil, Shea Fruit Butter, Cocoa Powder, Coffee Grounds"
   },
   {
     id: 7,
     name: "Romantic Rose Garden",
     price: 13.99,
     category: "Floral",
+    scent: "Rose",
     image: "images/products/lufarose.jpg",
     description: "A luxurious rose-infused bar crafted with floral oils and gentle clays for a soft, elegant cleanse.",
-    ingredients: "Olive Oil, Coconut Oil, Castor Oil, Palm Oil, Ginger Water, Essential Oil, Pink Clay, Sodium Hydroxide"
+    ingredients: "Olive Oil, Coconut Oil, Castor Oil, Palm Oil, Ginger Water, Pink Clay"
   },
   {
     id: 8,
     name: "Mugwort Herbal Shampoo Bar",
     price: 12.99,
     category: "Herbal",
+    scent: "Mint",
     image: "images/products/herb.jpg",
     description: "A refreshing mugwort shampoo bar that clarifies the scalp, reduces buildup, and leaves hair feeling light and clean.",
-    ingredients: "Sodium Cocoyl Isethionate, Sodium Lauryl Sulfoacetate, Mugwort Powder, Essential Oil, Castor Oil"
+    ingredients: "Sodium Cocoyl Isethionate, Sodium Lauryl Sulfoacetate, Mugwort Powder, Castor Oil"
   },
   {
     id: 9,
     name: "Ocean Mint Salt Bar",
     price: 11.99,
     category: "Natural",
+    scent: "Sweet Orage, Min, Lemon Grass,",
     image: "images/products/oceansalt.jpg",
     description: "Inspired by the ocean breeze, this cooling mint bar blends sea salt with refreshing botanicals for a smooth, invigorating cleanse.",
-    ingredients: "Olive Oil, Coconut Oil, Salt, Milk, Essential Oil, Sodium Hydroxide"
+    ingredients: "Olive Oil, Coconut Oil, Pink Salt, Milk"
   },
   {
     id: 10,
     name: "Citrus Sunrise",
     price: 11.99,
     category: "Citrus",
+    scent: "Orange, Mint",
     image: "images/products/orange.jpg",
     description: "A bright and refreshing orange soap that energizes the skin with a sweet citrus aroma.",
-    ingredients: "Olive Oil, Coconut Oil, Salt, Turmic Powder, Orange Zest, Essential Oil, Sodium Hydroxide"
+    ingredients: "Olive Oil, Coconut Oil, Salt, Turmic Powder, Orange Zest"
   },
   {
     id: 11,
     name: "Purifying Charcoal Detox",
     price: 14.99,
     category: "Specialty",
+    scent: "Lemongrass",
     image: "images/products/charcoal.jpg",
     description: "A powerful deep-cleansing bar enriched with activated charcoal to draw out impurities and refresh the skin.",
-    ingredients: "Olive Oil, Coconut Oil, Shea Butter, Essential Oils, Bamboo Charcoal Powder, Sodium Hydroxide"
+    ingredients: "Olive Oil, Coconut Oil, Shea Butter, Essential Oils, Bamboo Charcoal Powder"
   }
 ];
